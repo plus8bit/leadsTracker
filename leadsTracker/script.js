b5541763cbd0f892fb4bd7a20780f5d7d12f5ee9
Nick <plus0bit@gmail.com>
2023-07-07 10:50:52 +0300

    Add render function

--- leadsTracker/script.js
+++ leadsTracker/script.js
@@ -1,9 +1,17 @@
 const myLeads = []
-const inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
 document.getElementById('input-btn').addEventListener('click', () => {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  renderLeads()
 })
 
+function renderLeads() {
+  let listItems = ''
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>${myLeads[i]}</li>`
+  }
+
+  ulEl.innerHTML = listItems
+}
