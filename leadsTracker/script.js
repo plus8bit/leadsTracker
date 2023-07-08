7884f87caf00415cef2a5ebeffdfcc171d07029b
Nick <plus0bit@gmail.com>
2023-07-08 16:33:31 +0300

    refactoring code

--- leadsTracker/script.js
+++ leadsTracker/script.js
@@ -7,31 +7,31 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
-  renderLeads()
+  render(myLeads)
 }
 
-deleteBtn.addEventListener('dblclick', () => {
-  localStorage.clear()
-  myLeads = []
-  renderLeads()
-})
-
-document.getElementById('input-btn').addEventListener('click', () => {
-  myLeads.push(inputEl.value)
-  inputEl.value = ''
-  localStorage.setItem('myLeads', JSON.stringify(myLeads))
-  renderLeads()
-})
-
-function renderLeads() {
+function render(leads) {
   let listItems = ''
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a href="${myLeads[i]}" target="_blank" >${myLeads[i]}</a>
+        <a href="${leads[i]}" target="_blank" >${leads[i]}</a>
       </li>
     `
   }
 
   ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener('dblclick', () => {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+document.getElementById('input-btn').addEventListener('click', () => {
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  render(myLeads)
+})
